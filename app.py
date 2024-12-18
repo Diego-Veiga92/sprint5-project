60838f867280db1405b0c37bd7d0e12dd4bfd9fd
Diego-Veiga92 <peleteiro62@gmail.com>
2024-12-18 19:30:18 +0000

    adição de gráfico de dispersão em app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import plotly.express as px
 
 car_data = pd.read_csv(
     'vehicles.csv')  # lendo os dados
-# criando botão
+# criando botões
 hist_button = st.button('Criar histograma')
+disp_button = st.button('Criar gráfico de dispersão')
 
 if hist_button:  # se o botão for clicado
     # escrever uma mensagem
@@ -14,6 +15,16 @@ if hist_button:  # se o botão for clicado
 
     # criar um histograma
     fig = px.histogram(car_data, x="odometer")
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
+if disp_button:  # se o botão for clicado
+    # escrever uma mensagem
+    st.write(
+        'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+
+    # criar um gráfico de dispersão
+    fig = px.scatter(car_data, x="odometer")
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
