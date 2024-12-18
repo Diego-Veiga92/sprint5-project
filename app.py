a5c01853649720c834a217a8fa08a3d04f91811a
Diego-Veiga92 <peleteiro62@gmail.com>
2024-12-18 18:57:23 +0000

    cretae config.toml

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import plotly_express as px
 
 car_data = pd.read_csv(
     r'C:\Users\aswit\sprint5-project\vehicles.csv')  # lendo os dados
+
 if hist_button:  # se o botão for clicado
     # escrever uma mensagem
     st.write(
